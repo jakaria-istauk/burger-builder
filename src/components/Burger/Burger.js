74605bee1e4594c0473aec81548672cbc853834a
jakariaistauk <jakariamd35@gmail.com>
2020-04-21 16:37:34 +0600

    empty imgredients message t-130

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,19 @@ import BurgerIngredient from './Burgerinfredient/Burgerinfredient';
 
 const burger = (props) => {
 
-    const ingredient = Object.keys(props.ingredients)
+    let ingredient = Object.keys(props.ingredients)
                     .map( igKey =>{                        
                         return [...Array(props.ingredients[igKey])].map((_, i) => {
                             return <BurgerIngredient key={ igKey + i } type={igKey} />;
                         });
-                    } );
+                    } )
+                    .reduce((arr, el) =>{
+                        return arr.concat(el)
+                    },[]);
+
+    if( ingredient.length === 0 ){
+        ingredient = <p>Please Start adding ingredients!</p>;
+    }
     
     return(
         <div className={classes.Burger}>
