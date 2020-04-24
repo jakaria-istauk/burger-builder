8e1f7604c398d9ef58cb1d069fcd9b23a005d072
jakariaistauk <jakariamd35@gmail.com>
2020-04-24 11:29:48 +0600

    order now button t-136

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -23,7 +23,20 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 30
+        totalPrice: 30,
+        purchasable: false
+    }
+
+    updatePurchase(ingredients){
+        const sum = Object.keys(ingredients)
+            .map( igKey => {
+                return ingredients[igKey];
+            } )
+            .reduce( (sum, el) =>{
+                return sum + el;
+            },0);
+
+            this.setState({purchasable: sum > 0 });
     }
 
     addIngredient = (type) => {
@@ -37,6 +50,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchase(updatedIngredients);
     }
 
     removeIngredient = (type) => {
@@ -53,6 +67,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - priceDeduction;
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchase(updatedIngredients);
     }
 
     render(){
@@ -69,6 +84,7 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.addIngredient} 
                     ingredientRemoved={this.removeIngredient} 
                     disabled={this.disabledInfo} 
+                    purchasable={this.state.purchasable} 
                     price={this.state.totalPrice} />
             </Aux>
         );
